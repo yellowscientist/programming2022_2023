ce80c0dda79b32abafd48c342cc9620264036096
yellowscientist <reddan1470@gmail.com>
2022-09-29 10:33:26 +0300

    revers arr

--- ArraysActions_Easy/arrays_easy.c
+++ ArraysActions_Easy/arrays_easy.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <math.h>
 
 int main() {
 
@@ -9,7 +9,7 @@ int main() {
 
 	int sizeOfArr;
 
-	printf("enter size of array: ");
+	printf("enter size of array (max - 100): ");
 	scanf_s("%d", &sizeOfArr);
 
 	int arr[100];
@@ -19,8 +19,39 @@ int main() {
 		arr[i] = -10 + rand() % 21;
 		printf("%d ", arr[i]);
 	}
-
 	printf("\n");
+
+	int userNum, userDegr;
+
+	printf("enter any num and num from 2 to 5: ");
+	scanf_s("%d %d", &userNum, &userDegr);
+
+	for (int i = 0; i < sizeOfArr; i++)
+	{
+		if (arr[i] % userNum == 0) {
+			arr[i] = pow(arr[i], userDegr);
+		}
+	}
+
+	int arrClone[100];
+
+	for (int i = sizeOfArr; i >= 0 ; i--)
+	{
+		arrClone[sizeOfArr - i - 1] = arr[i];
+	}
+
+	for (int i = 0; i < sizeOfArr; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < sizeOfArr; i++)
+	{
+		printf("%d ", arrClone[i]);
+	}
+	printf("\n");
+
 	system("pause");
 	return 0;
 
