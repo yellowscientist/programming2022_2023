15057bf3305cc6f8f6bfd887a23f1d63ecf070f3
yellowscientist <reddan1470@gmail.com>
2022-09-29 10:02:18 +0300

    create array and fill it rand nums

--- ArraysActions_Easy/arrays_easy.c
+++ ArraysActions_Easy/arrays_easy.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main() {
 
+	srand(time(0));
 
+	int sizeOfArr;
+
+	printf("enter size of array: ");
+	scanf_s("%d", &sizeOfArr);
+
+	int arr[100];
+
+	for (int i = 0; i < sizeOfArr; i++)
+	{
+		arr[i] = -10 + rand() % 21;
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
 	system("pause");
 	return 0;
 
